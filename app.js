e1a2768b0fa88d3ea204d5af3aed490391d18fa2
Aman Sharma <99912835+Aman-Sharma1221@users.noreply.github.com>
2022-04-08 22:22:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,19 +33,19 @@ function getResult() {
         result = 'its a draw!'
     }
     if (computerChoice === 'rock' && userChoice === "paper") {
-        result = 'you win 🎊'
+        result = 'you win!'
     }
     if (computerChoice === 'rock' && userChoice === "scissors") {
         result = 'you lost!'
     }
     if (computerChoice === 'paper' && userChoice === "scissors") {
-        result = 'you win 🎊'
+        result = 'you win!'
     }
     if (computerChoice === 'paper' && userChoice === "rock") {
         result = 'you lose!'
     }
     if (computerChoice === 'scissors' && userChoice === "rock") {
-        result = 'you win 🎊'
+        result = 'you win!'
     }
     if (computerChoice === 'scissors' && userChoice === "paper") {
         result = 'you lose!'
